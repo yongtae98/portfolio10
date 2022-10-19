09f3cad078d216727903f546d8b6df2fe3ff97de
yongtae98 <choiyt3939@naver.com>
2022-10-20 02:12:20 +0900

    add dist file

---
+++ js/about.ba80a440.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkportfolio"]=self["webpackChunkportfolio"]||[]).push([[443],{6506:function(n,t,o){o.r(t),o.d(t,{default:function(){return l}});var r=o(3396);const u={class:"about"},a=(0,r._)("h1",null,"This is an about page",-1),e=[a];function s(n,t){return(0,r.wg)(),(0,r.iD)("div",u,e)}var i=o(89);const c={},f=(0,i.Z)(c,[["render",s]]);var l=f}}]);
+//# sourceMappingURL=about.ba80a440.js.map
